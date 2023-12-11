ba38b8e9cc756bc4aab8baaaaaa919a848f74d61
Zsolt Tasnadi <rastasi@gmail.com>
2023-12-11 20:13:52 +0100

    fix http controller

--- apps/python_service/app/http/controllers/todo_controller.py
+++ apps/python_service/app/http/controllers/todo_controller.py
@@ -11,11 +11,11 @@ class TodoController:
 
     def index(self) -> Response:
         todos = self.todo_service.get_all()
-        return todos
+        return todos, 200
 
     def show(self, todo_id: int) -> Response:
         todo = self.todo_service.get(todo_id)
-        return todo
+        return todo, 200
 
     def create(self) -> Response:
         payload = request.get_json(force=True)
@@ -24,7 +24,7 @@ class TodoController:
             description=payload['description'],
         )
         todo = self.todo_service.create(todo_dto)
-        return todo
+        return todo, 201
 
     def update(self, todo_id: int) -> Response:
         payload = request.get_json()
@@ -33,8 +33,8 @@ class TodoController:
             description=payload['description'],
         )
         todo = self.todo_service.update(todo_id, todo_dto)
-        return todo
+        return todo, 200
 
     def destroy(self, todo_id: int) -> Response:
         self.todo_service.destroy(todo_id)
-        return Response(status=204)
+        return "No content", 204
